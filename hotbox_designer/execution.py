ac28f60d57c8ee7bb18054972061f8f2d3171807
PyLio <lionel.brouyere@gmail.com>
2018-11-12 12:21:18 +0100

    fixe languages

--- hotbox_designer/execution.py
+++ hotbox_designer/execution.py
@@ -1,15 +1,19 @@
 
 
-EXECUTORS = {
-    'mel': execute_mel,
-    'python': exec
-}
-
-
 def execute_code(language, code):
     return EXECUTORS[language](code)
+
+
+def execute_python(code):
+    exec(code)
 
 
 def execute_mel(code):
     from maya import mel
     mel.eval(code.replace(u'\u2029', '\n'))
+
+
+EXECUTORS = {
+    'mel': execute_mel,
+    'python': execute_python
+}
