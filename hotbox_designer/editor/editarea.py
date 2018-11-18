cfc47a7d921d5c10dca35d61cf242b69c0b3a845
luckylyk <lionel.brouyere@gmail.com>
2018-11-19 00:21:28 +0100

    /feature/ selection improved in editor
    selection is improved, it possible to select a shape if a shape under it
    is selected

--- hotbox_designer/editor/editarea.py
+++ hotbox_designer/editor/editarea.py
@@ -27,6 +27,7 @@ class ShapeEditArea(QtWidgets.QWidget):
         self.clicked_shape = None
         self.clicked = False
         self.handeling = False
+        self.manipulator_moved = False
         self.edit_center_mode = False
         self.increase_undo_on_release = False
 
@@ -56,6 +57,7 @@ class ShapeEditArea(QtWidgets.QWidget):
         if self.handeling is False:
             return self.repaint()
 
+        self.manipulator_moved = True
         rect = self.manipulator.rect
         if self.transform.direction:
             self.transform.resize([s.rect for s in self.selection], cursor)
@@ -77,6 +79,7 @@ class ShapeEditArea(QtWidgets.QWidget):
         self.clicked = True
         self.transform.direction = direction
 
+        self.manipulator_moved = False
         rect = self.manipulator.rect
         if rect is not None:
             self.transform.set_rect(rect)
@@ -104,7 +107,11 @@ class ShapeEditArea(QtWidgets.QWidget):
             return
 
         shape = self.clicked_shape
-        if not self.handeling:
+        selection_update_conditions = (
+            self.handeling is False
+            or shape not in self.selection
+            and self.manipulator_moved is False)
+        if selection_update_conditions:
             self.selection.set([shape] if shape else None)
             self.update_selection()
 
