7cbb3f3a7d913763f7c5268434ecfd450ebb1c9c
luckylyk <lionel.brouyere@gmail.com>
2018-12-17 21:41:07 +0100

    load_json putted back to init

--- hotbox_designer/__init__.py
+++ hotbox_designer/__init__.py
@@ -1,6 +1,6 @@
 import os
 import json
 from hotbox_designer.reader import HotboxWidget
-from hotbox_designer.data import load_templates
+from hotbox_designer.data import load_templates, load_json
 from hotbox_designer.manager import (
     launch_manager, initialize, show, hide, switch, load_hotboxes)
